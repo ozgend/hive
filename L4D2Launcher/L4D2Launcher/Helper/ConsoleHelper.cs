8027cd7c923d5ba846b97f21d13c0a77086f9695
denolk <revolter00@gmail.com>
2013-02-13 19:23:29 +0200

    command & arguments fixed.

--- L4D2Launcher/L4D2Launcher/Helper/ConsoleHelper.cs
+++ L4D2Launcher/L4D2Launcher/Helper/ConsoleHelper.cs
@@ -14,15 +14,16 @@ namespace L4D2Launcher.Helper
     {
         public static void Execute(Parameters p)
         {
-            string command = Build(p);
-            new Thread(() => _Execute(command)).Start();
+            string target = string.Format("{0}\\{1}",Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), Parameters.TargetName);
+            string arguments = Build(p);
+            new Thread(() => _Execute(target,arguments)).Start();
 
         }
 
-        private static void _Execute(string command)
+        private static void _Execute(string target, string arguments)
         {
             Process p = new Process();
-            ProcessStartInfo startInfo = new ProcessStartInfo(command);
+            ProcessStartInfo startInfo = new ProcessStartInfo(target,arguments);
             startInfo.Verb = "runas";
             p.StartInfo = startInfo;
             p.Start();
@@ -31,19 +32,11 @@ namespace L4D2Launcher.Helper
         private static string Build(Parameters p)
         {
             List<ArgModel> args = p.ConsoleArgs();
-
             StringBuilder sb = new StringBuilder();
-
-            sb.Append(Path.GetFullPath(
-                string.Format(
-                    "{0}\\{1}",
-                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), Parameters.TargetName)
-                )
-            );
 
             foreach (var arg in args)
             {
-                sb.AppendFormat("{0} ", arg.ToString());
+                sb.AppendFormat(" {0}", arg.ToString());
             }
             return sb.ToString();
         }
